daabbfd10db3dcddf558cdb3c1be06e4eb968db5
Johnny <developer94@gmail.com>
2013-07-14 21:42:43 +0200

    Add additional newlines. Handle invalid choice.

--- rps-tests/rps-run.py
+++ rps-tests/rps-run.py
@@ -1,4 +1,4 @@
-from rps_oop import Player, AI, RollHandler
+from rps_oop import Player, AI, RollHandler, InvalidRollOptionError
 
 if __name__ == "__main__":
     player = Player()
@@ -7,8 +7,15 @@ if __name__ == "__main__":
     question = 'yes'
 
     while question != 'n':
-        print player.name + " chooses " + player.Input().name + "."
-        print ai.name + " chooses " + ai.Roll().name + "."
-        print RollHandler.Weigh(player, ai)
+        try:
+            print player.name + " chooses " + player.Input().name + "."
+            print ai.name + " chooses " + ai.Roll().name + "."
+            print RollHandler.Weigh(player, ai)
+        except InvalidRollOptionError:
+            print "Invalid choice. Please choose from rock/paper/scissors."
+
+        print
 
         question = raw_input("Do you wanna go for another roll? [y]/n: ")
+
+        print
